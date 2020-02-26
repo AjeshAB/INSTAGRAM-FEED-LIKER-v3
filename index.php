709c83c2ab06e9bb75f320b1c7375ab5158bb882
Ajesh Ab <60130404+D3V1Lh4x0r@users.noreply.github.com>
2020-02-26 20:35:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+set_time_limit(0);
+include 'Instagram.class.php';
+clear();
+echo "
+ *  INSTAGRAM FEED LIKER [v3]
+ *  STATUS @BETA
+ *  AUTHOR @DBGRAM
+ *  WHATSAPP  +917012045385
+ *  RECOMMENDED SLEEP 60s
+  
+    •••••••••••••••••••••••••••••••••••••••••
+    
+ * Use tools at your own risk.
+ * Use this Tool for personal use, not for sale.
+ * I am not responsible for your account using this tool.
+ * Make sure your account has been verified (Email & Telp).
+ 
+";
+$username    = getUsername();
+$password    = getPassword();
+$sleep = rand(0,10) + getComment('[?]  Sleep in Seconds: ');
+echo '•••••••••••••••••••••••••••••••••••••••••' . PHP_EOL . PHP_EOL;
+$login = login($username, $password);
+if ($login['status'] == 'success') {
+    echo '[*] Login as ' . $login['username'] . ' Success!' . PHP_EOL;
+    $data_login = array(
+        'username' => $login['username'],
+        'csrftoken' => $login['csrftoken'],
+        'sessionid' => $login['sessionid']
+    );
+    while (true) {
+        $profile    = getHome($data_login);
+        $data_array = json_decode($profile);
+        $result     = $data_array->user->edge_web_feed_timeline;
+        foreach ($result->edges as $items) {
+            $id       = $items->node->id;
+            $username = $items->node->owner->username;
+            $like     = like($id, $data_login);
+            if ($like['status'] == 'error') {
+                echo '[+] Username: ' . $username . ' | Media_id: ' . $id . ' | Error Like' . PHP_EOL;
+                logout($data_login);
+                $login = login($username, $password);
+                if ($login['status'] == 'success') {
+                    echo '[*] Login as ' . $login['username'] . ' Success!' . PHP_EOL;
+                    $data_login = array(
+                        'username' => $login['username'],
+                        'csrftoken' => $login['csrftoken'],
+                        'sessionid' => $login['sessionid']
+                    );
+                }else{
+
+                    die("Something went wrong");
+
+                }
+            } else {
+                echo '[+] Username: ' . $username . ' | Media_id: ' . $id . ' | Like Success' . PHP_EOL;
+            }
+            break;
+        }
+        echo '[+] [' . date("H:i:s") . '] Sleep for ' . $sleep . ' seconds [+]' . PHP_EOL;
+        sleep( $sleep);
+        echo '•••••••••••••••••••••••••••••••••••••••••' . PHP_EOL . PHP_EOL;
+    }
+
+} else
+
+    echo json_encode($login);
